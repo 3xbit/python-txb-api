086bcf8ccb3289409371dccdb9f0887e0a193abf
Deny <dl.rodrigues94@gmail.com>
2019-02-19 15:08:51 -0300

    Include MIT License

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version='1.0.0',
     packages=find_packages(),
     url='https://github.com/3xbit/python-txb-api',
-    license='',
+    license='MIT License',
     author='3xBit',
     author_email='drodrigues@3xbit.com.br',
     description='Python wrapper for 3xBit API',
